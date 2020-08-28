7089f1b77b393a4fa5144bf6600f20b4b0f97dd6
amrfahmy5 <elkingamr46@yahoo.com>
2020-08-28 09:07:25 +0200

    local-fb passport

--- app/models/User.js
+++ app/models/User.js
@@ -1,14 +1,29 @@
 const con = require("../../config/connection");
 
 
-exports.singnup = (req,callback)=>{
+exports.singnup = (req, callback) => {
     con.query(
         "INSERT INTO user" +
         " (user_name, email,password,age,role,img)" +
-        " VALUES ('"+req.name+"','"+req.email+"','"+req.password+"','"+req.age+"','"+req.role+"','"+req.img+"')" ,callback)
+        " VALUES ('" + req.name + "','" + req.email + "','" + req.password + "','" + req.age + "','" + req.role + "','" + req.img + "')", callback)
+}
+exports.login = (user, callback) => {
+    con.query('SELECT * FROM  user WHERE email = ? AND password = ?',
+        [user.email, user.password], callback)
 }
 
-exports.login = (user,callback)=>{
-    con.query('SELECT * FROM  user WHERE email = ? AND password = ?',
-            [user.email, user.password],callback)
+exports.fbSingnup = (req, callback) => {
+    con.query(
+        "INSERT INTO user (facebookID,user_name, email,password,accessToken,age,role,img) VALUES (?,?,?,?,?,'1','1','img')"
+        , [req.id, req.name, req.email, req.password, req.password], callback)
+}
+
+exports.fbLogin = (id, callback) => {
+    con.query('SELECT * FROM  user WHERE facebookID = ?',
+        [id], callback)
+}
+
+exports.userSession = (id, callback) => {
+    con.query('SELECT * FROM  user WHERE id = ?',
+        [id], callback)
 }
